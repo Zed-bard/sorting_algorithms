ab7c60db7454ea37386faf5e923e0e79d46355b7
jemi4jo <jemi4jo@gmail.com>
2024-01-23 20:23:50 +0300

    selection 1

---
+++ 2-selection_sort.c
@@ -0,0 +1,27 @@
+#include "sort.h"
+
+void selection_sort(int *array, size_t size)
+{
+    size_t i, j, min_idx;
+
+    for (i = 0; i < size - 1; i++)
+    {
+        min_idx = i;
+        for (j = i + 1; j < size; j++)
+        {
+            if (array[j] < array[min_idx])
+            {
+                min_idx = j;
+            }
+        }
+
+        if (min_idx != i)
+        {
+            /* Swap the found minimum element with the first element */
+            int temp = array[min_idx];
+            array[min_idx] = array[i];
+            array[i] = temp;
+            print_array(array, size); /* Print the array after each swap */
+        }
+    }
+}
